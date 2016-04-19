3b3cd13f9600da925ba262a284b9c2a3b3f6650d
Sergei Tumanov <tumanser@gmail.com>
2016-04-19 13:12:25 +0300

    Рефакторинг.

--- src/gs_test/src/main/java/tuman/gs_test/Acuario.java
+++ src/gs_test/src/main/java/tuman/gs_test/Acuario.java
@@ -12,41 +12,85 @@ import java.util.Arrays;
  */
 public class Acuario {
 
+	/**
+	 * Колонка аквариума.
+	 * @author Sergei Tumanov.
+	 */
+	private static class Column {
+
+		/** Количество клеток основания. */
+		public int ground;
+		/** Количество клеток воды. */
+		public int water;
+
+		/**
+		 * Создание экземпляра.
+		 * @param ground Количество клеток основания.
+		 * @param water Количество клеток воды.
+		 */
+		public Column(int ground, int water) {
+			this.ground = ground;
+			this.water = water;
+		}
+
+	}
+
+
+
 	/** Глубина аквариума. */
 	private int depth;
 	/** Содержимое. */
-	private int[][] contents;
-	
-	
-	
+	private Column[] contents;
+
+
+
+	/**
+	 * Создание кземпляра.
+	 */
+	public Acuario() {}
+
 	/**
 	 * Создание кземпляра.
 	 * @param bottom высоты дна.
 	 */
 	public Acuario(int[] bottom) {
-		depth = Arrays.stream(bottom).reduce(0, (max, d) -> d > max ? d : max);
-		contents = Arrays.stream(bottom)
-				.mapToObj(b -> new int[] {b >= 0 ? b : 0, depth - (b >= 0 ? b : 0)})
-				.toArray(int[][]::new);
-		System.out.println("Depth: " + depth);
+		setBottom(bottom);
+		fill();
 	}
-	
-	
-	
+
+
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < depth; i++) {
 			int h = depth - i - 1;
-			for (int j = 0, n = contents.length; j < n; j++) {
-				int b = contents[j][0];
-				int a = contents[j][1];
-				System.err.printf("SA: %d, %d, %d\n", h, b, a);
-				sb.append(h >= b + a ? '.' : h >= b ? '~' : '#');
-			}
+			for (Column column: contents)
+				sb.append(h >= column.ground + column.water ? '.' : h >= column.ground ? '~' : '#');
 			sb.append('\n');
 		}
 		return sb.toString();
 	}
-	
+
+
+
+	/**
+	 * Задать основание.
+	 * @param bottom высоты основания.
+	 */
+	public void setBottom(int[] bottom) {
+		depth = Arrays.stream(bottom).max().orElse(0);
+		contents = Arrays.stream(bottom)
+				.mapToObj(b -> new Column(b >= 0 ? b: 0, 0))
+				.toArray(Column[]::new);
+	}
+
+	/**
+	 * Заполнить аквариум водой до верху.
+	 */
+	public void fill() {
+		for (Column column: contents)
+			column.water = depth - column.ground;
+	}
+
 }
